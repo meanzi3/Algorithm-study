5fec0467613d32b5d351ae5b73e78cec634b153a
Minji <120402129+meanzi3@users.noreply.github.com>
2023-11-30 17:02:50 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.20 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -5,13 +5,16 @@ class Solution {
         new_id = new_id.toLowerCase();
         
         // 2단계
-        new_id = new_id.replaceAll("[^a-z0-9\\-_.]", "");
+        // new_id = new_id.replaceAll("[^a-z0-9\\-_.]", "");
+        // 백스페이스는 자바에서도 escape sequence 역할을 하기 때문에 정규 표현식에 제대로 된 백스페이스를 넣어주려면 자바에서도 백스페이스를 이용하여 백스페이스 문자를 문자열에 포함해주어야 한다. (2번 쓴 이유)
+        new_id = new_id.replaceAll("[^a-z0-9._-]", "");
         
         // 3단계
-        new_id = new_id.replaceAll("\\.{2,}", ".");
+        // [] 패턴으로 감싸면, 내부의 문자 중 하나라는 의미로, 백스페이스를 붙여주지 않아도 된다.
+        new_id = new_id.replaceAll("[.]{2,}", ".");
         
         // 4단계
-        new_id = new_id.replaceAll("^\\.+|\\.+$","");
+        new_id = new_id.replaceAll("^[.]|[.]$","");
         
         // 5단계
         if(new_id.equals("")) new_id = "a";
@@ -22,7 +25,7 @@ class Solution {
             new_id = new_id.substring(0, 15);
             
             // 마침표가 끝에 있으면 제거
-            new_id = new_id.replaceAll("\\.+$","");
+            new_id = new_id.replaceAll("[.]$","");
         }
         
         // 7단계
