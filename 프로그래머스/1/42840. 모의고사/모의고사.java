4852a98e36f93456584d62c58e844dfc7f768569
Minji <120402129+meanzi3@users.noreply.github.com>
2023-12-04 16:21:09 +0900

    [level 1] Title: 모의고사, Time: 4.67 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+    
+    private static final int[][] Rule = {
+        {1, 2, 3, 4, 5},
+        {2, 1, 2, 3, 2, 4, 2, 5},
+        {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+    };
+    
+    private int getAnswers(int person, int problem){
+        int[] rule = Rule[person];
+        int index = problem % rule.length;
+        return rule[index];
+    }
+    
+    public int[] solution(int[] answers) {
+                
+        int score[] = new int[3]; // 맞힌 갯수 저장
+        int max = 0;
+        
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < answers.length; j++){
+                if(getAnswers(i, j) == answers[j]){
+                  score[i]++;
+                } 
+            }
+            if(score[i] > max) max = score[i];
+        }
+    
+        // score 배열 돌면서 max인 사람 찾아서 answer 배열에 저장
+        List<Integer> answerList = new ArrayList<>();
+        for(int i = 0; i < 3; i++){
+            // 여러 명이면?? return 값을 오름차순 정렬.....
+            if(score[i] == max){
+                answerList.add(i + 1);
+            }
+        }
+        
+        int[] answer = answerList.stream()
+            .mapToInt(i -> i)
+            .toArray();
+        
+        return answer;
+    }
+}
