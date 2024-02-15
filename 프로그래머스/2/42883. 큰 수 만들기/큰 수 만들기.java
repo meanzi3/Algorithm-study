336651d477660e312c44bb5b325693921d0658ef
Minji <120402129+meanzi3@users.noreply.github.com>
2024-02-16 00:59:43 +0900

    [level 2] Title: 큰 수 만들기, Time: 2.59 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public String solution(String number, int k) {
+        String answer = "";
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for(char c : number.toCharArray()){
+            while(k > 0 && !stack.isEmpty() && c > stack.peek()){
+                stack.pop();
+                k--;
+            }
+            stack.push(c);
+        }
+        while(k-- > 0){
+            stack.pop();
+        }
+        
+        return stack.stream()
+            .map(String::valueOf)
+            .collect(Collectors.joining());
+    }
+}
