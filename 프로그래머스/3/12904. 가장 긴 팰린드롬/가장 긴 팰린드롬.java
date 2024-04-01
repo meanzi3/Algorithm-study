d1d7e630e92ccda41a911c43d104fa5b88bb7185
Minji <minjichoe0326@gmail.com>
2024-04-01 18:48:34 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 26.83 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -1,32 +1,25 @@
-class Solution
-{
-    public int solution(String s)
-    {
-        int answer = 0;
+class Solution {
+    public int solution(String s) {
+        int maxLength = 0;
         int n = s.length();
         
-        loop: 
-        for(int i = n; i >= 1; i--){ // 가장 긴 길이부터 살펴보기
-            for(int j = 0; j <= n - i; j++){
-               boolean flag = true;
-               int st = j; //시작 인덱스 
-               int en = j+i-1; // 끝 인덱스 
-               
-               while(st < en){ 
-                   if(s.charAt(st) != s.charAt(en)){
-                       flag = false;
-                       break;
-                   }
-                   st++;
-                   en--;
-               }
-               if(flag){ // 팰린드롬이면 정답 갱신하고 반복문 중단하기 
-                   answer = i;
-                   break loop;
-               }
-            }
-               
+        for (int i = 0; i < n; i++) {
+            // 팰린드롬이 홀수 길이인 경우
+            int len1 = expandAroundCenter(s, i, i);
+            // 팰린드롬이 짝수 길이인 경우
+            int len2 = expandAroundCenter(s, i, i + 1);
+            maxLength = Math.max(maxLength, Math.max(len1, len2));
         }
-        return answer;
+        
+        return maxLength;
+    }
+    
+    private int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        // 팰린드롬이 끝날 때 left와 right는 실제로 한 칸씩 더 나아가게 되므로 (right - 1) - (left + 1) + 1 = right - left - 1의 길이가 팰린드롬의 길이
+        return right - left - 1;
     }
 }
