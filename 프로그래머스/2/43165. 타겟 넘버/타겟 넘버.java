8a4468dfc3d0e2bb9a3bcd93c98381049aae75a4
Minji <minjichoe0326@gmail.com>
2024-07-26 16:11:57 +0900

    [level 2] Title: 타겟 넘버, Time: 8.82 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,30 +1,27 @@
 class Solution {
-    
-    static int answer = 0;
+    private static int answer = 0;
     
     public int solution(int[] numbers, int target) {
         
-        // 배열, depth, target 숫자, 지금까지 만든 숫자로 dfs한다.
-        dfs(numbers, 0, target, 0);
+        // depth, target, numbers, 만든 숫자 num
+        dfs(0, target, numbers, 0);
         
         return answer;
     }
     
-    private static void dfs(int[] numbers, int depth, int target, int result){
-        // 배열 끝까지 가면 종료
+    private void dfs(int depth, int target, int[] numbers, int num) {
+        // 종료 조건
         if(depth == numbers.length){
-            if(result == target){
-                // 만든 숫자가 target 숫자와 같으면 answer + 1
-                answer ++;
+            // 만든 수가 target이라면 경우의 수 ++
+            if(num == target){
+                answer++;
+                return;
             }
-            // 종료
-            return ;
+            return;
         }
         
-        // 다음 숫자를 더했을 때
-        dfs(numbers, depth+1, target, result + numbers[depth]);
-        // 다음 숫자를 뺏을 때
-        dfs(numbers, depth+1, target, result - numbers[depth]);
-        
+        // 더하거나 뺴거나
+        dfs(depth + 1, target, numbers, num + numbers[depth]);
+        dfs(depth + 1, target, numbers, num - numbers[depth]);
     }
 }
