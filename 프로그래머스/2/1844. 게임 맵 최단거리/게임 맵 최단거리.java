af8ac08bd7c427b2eb6ed9d32ed6bae34dd9819c
Minji <minjichoe0326@gmail.com>
2024-07-26 16:43:14 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.87 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -3,16 +3,16 @@ import java.util.*;
 class Solution {
     
     private static int[] dr = {-1, 0, 1, 0};
-    private static int[] dc = {0, -1, 0, 1};
+    private static int[] dc = {0, 1, 0, -1};
     
     private static int n;
     private static int m;
     
     private static boolean[][] visited;
-    
     private static int answer = -1;
     
     public int solution(int[][] maps) {
+        
         n = maps.length;
         m = maps[0].length;
         visited = new boolean[n][m];
@@ -23,18 +23,19 @@ class Solution {
     }
     
     private static int bfs(int[][] maps){
+        
         Queue<int[]> q = new LinkedList<int[]>();
         
         visited[0][0] = true;
-        q.add(new int[] {0, 0, 1});
+        q.offer(new int[] {0, 0, 1});
         
-        while(!q.isEmpty()){
-            int[] temp = q.poll();
-            int r = temp[0];
-            int c = temp[1];
-            int count = temp[2];
+        while(!q.isEmpty()) {
+            int tmp[] = q.poll();
+            int r = tmp[0];
+            int c = tmp[1];
+            int count = tmp[2];
             
-            if(r == n-1 && c == m-1) {
+            if(r == n-1 && c == m-1){
                 answer = count;
                 break;
             }
@@ -43,13 +44,17 @@ class Solution {
                 int nr = r + dr[i];
                 int nc = c + dc[i];
                 
-                if(nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
-                if(maps[nr][nc] == 0) continue;
-                if(!visited[nr][nc]){
-                    visited[nr][nc] = true;
-                    q.add(new int[] {nr, nc, count+1});
-                }
-            }
+                // 범위, 방문 여부 확인
+                if(nr < 0 || nr >= n || nc < 0 || nc >= m || visited[nr][nc])
+                    continue;
+                
+                // 길인지 확인
+                if(maps[nr][nc] == 0)
+                    continue;
+                
+                visited[nr][nc] = true;
+                q.offer(new int[] {nr, nc, count + 1});
+            } 
         }
         
         return answer;
