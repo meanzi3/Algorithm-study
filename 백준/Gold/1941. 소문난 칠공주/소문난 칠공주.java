ed5511c2fe73d29753db2b8e6a6438c12c0a484c
Minji <minjichoe0326@gmail.com>
2025-01-21 21:31:47 +0900

    [Gold III] Title: 소문난 칠공주, Time: 340 ms, Memory: 67316 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -8,7 +8,6 @@ public class Main {
 	private static int[] dr = {-1, 0, 1, 0};
 	private static int[] dc = {0, 1, 0, -1};
 	private static boolean[] visited;
-	private static int[] selected;
 	
 	private static int answer = 0;
 	
@@ -25,10 +24,10 @@ public class Main {
 			students[i] = br.readLine().toCharArray();
 		} // 입력값 저장
 		
-		selected = new int[7];
+		visited = new boolean[25];
 		
 		// 백트래킹으로 7명 조합 만듦
-		dfs(0, 0, 0);
+		dfs(0, 0, 0, new ArrayList<>());
 		
 		// 출력
 		bw.write(answer + "\n");
@@ -38,40 +37,44 @@ public class Main {
 		
 	}
 	
-	private static void dfs(int depth, int numY, int start) {
+	private static void dfs(int depth, int numY, int start, List<Integer> selected) {
 		// 종료 조건
 		if(numY >= 4)	return;
 		
 		if(depth == 7) {
-			visited = new boolean[7];
-			bfs();
+			if(bfs(selected)) {
+				answer++;
+			}
 			return;
 		}
 		
 		for(int i = start; i < 25; i++) {
-			selected[depth] = i;
+			selected.add(i);
+			visited[i] = true;
 			
 			if(students[i/5][i%5] == 'Y') { // 임도연파일 때
-				dfs(depth + 1, numY + 1, i + 1);
+				dfs(depth + 1, numY + 1, i + 1, selected);
 			} else { // 임도연파가 아닐 때
-				dfs(depth + 1, numY, i + 1);
+				dfs(depth + 1, numY, i + 1, selected);
 			}
 			
-			selected[depth] = 0;
+			selected.remove(selected.size() - 1);
+			visited[i] = false;
 		}
 	}
 	
-	private static void bfs() {
-		Queue<int[]> q = new LinkedList<>();
-		q.offer(new int[] {selected[0] / 5, selected[0] % 5}); // 학생의 위치 (행, 열) 저장
-		visited[0] = true;
+	private static boolean bfs(List<Integer> selected) {
+		Queue<Integer> q = new LinkedList<>();
+		boolean[] check = new boolean[7];
+		q.offer(selected.get(0)); // 학생의 위치 (행, 열) 저장
+		check[0] = true;
 		
 		int cnt = 1; // 인접한 개수
 		
 		while(!q.isEmpty()) {
-			int[] temp = q.poll();
-			int r = temp[0];
-			int c = temp[1];
+			int temp = q.poll();
+			int r = temp / 5;
+			int c = temp % 5;
 			
 			for(int i = 0; i < 4; i++) {
 				int nr = r + dr[i];
@@ -79,18 +82,17 @@ public class Main {
 				int ni = nr * 5 + nc; // 다음 인덱스
 				
 				// 범위 확인
-				if(nr < 0 || nr >= 5 || nc < 0 || nc >= 5)	continue;
+				if(nr < 0 || nr >= 5 || nc < 0 || nc >= 5 || !selected.contains(ni))	continue;
 				
-				for(int j = 0; j < 7; j++) {
-					if(!visited[j] && selected[j] == ni) {
-						q.offer(new int[] {nr, nc});
-						visited[j] = true;
-						cnt++;
-					}
+				int idx = selected.indexOf(ni);
+				if(!check[idx]) {
+					check[idx] = true;
+					q.offer(ni);
+					cnt++;
 				}
 			}
 		}
 		
-		if(cnt == 7) answer++;
+		return cnt == 7;
 	}
 }
